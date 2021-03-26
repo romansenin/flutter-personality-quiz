423df5f6cb7c6e22707d718111772ae65fcae994
Roman Senin <roman@launchbadge.com>
2021-03-26 10:49:51 -0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  Widget build(context) {
+    return MaterialApp(
+      home: Text("Hello!"),
+    );
+  }
+}
